33ebb7f5dd4ad024315179384c8539efc0f78427
Jjosebalanta <jbalanta913@gmail.com>
2024-10-19 15:21:43 +0000

    parcial

---
+++ parcial4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+public class parcial4 {
+    public static void main(String[] args) {
+        String nombre;
+        double media;
+        double basic,pascal,fortran;
+        
+         Scanner leer= new Scanner(System.in);
+         System.out.println("ingresa los datos del alunno (para terminar escriba ***):");
+
+         
+          while (nombre!="***") {
+            System.out.println("ingresa en nombre del alumno");
+            nombre=leer.nextLine();
+          }
+    }
+}
